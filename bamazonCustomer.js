cd18105859170bbae6347e1c53ce0404f9df13c7
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-03 08:28:20 -0700

    finishing logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,32 +1,113 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-var connection = mysql.createConnection({
-  host: "localhost",
+const connection = mysql.createConnection({
+  host: 'localhost',
 
-  // Your port; if not 3306
+  // Your port; 
   port: 3306,
 
   // Your username
-  user: "root",
+  user: '',
 
   // Your password
-  password: "",
-  database: "bamazon"
+  password: '',
+  database: 'bamazon'
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("You are connected to your database");
-  queryAllItems();
+  console.log('You are connected to your database');
+  productTable();
+
 });
 
-function queryAllItems() {
-  connection.query('SELECT * FROM products', function (err, res) {
+function productTable() {
+  connection.query('SELECT * FROM products', function (err, bamanzonTable) {
     if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      console.log(`${res[i].id}. ${res[i].product_name}, ${res[i].department_name}, ${res[i].price}, ${res[i].stock_quantity}`)
-    };
+    // Shows the database table
+    console.table(bamanzonTable);
+    productID();
   });
 };
 
+// Prompt user for product ID
+function productID() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'itemId',
+        message: 'What is the id of the product you want to purchase?',
+      }
+    ])
+    .then(function (answer) {
+      console.log(answer)
+      // console.log('answer.itemId', answer.itemId);
+      verifyProduct(answer);
+    })
+
+}
+
+// Prompt user for product quantity
+function productQuantity(productData) {
+  // console.log(' productData', productData);
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many do you want to purchase?'
+      }
+    ])
+    .then(function (answer2) {
+      if (answer2.quantity > productData.stock_quantity) {
+        console.log('Insufficient product inventory!');
+        connection.end();
+
+      }
+      else productPurchase(productData, answer2.quantity)
+    })
+
+}
+
+// Check product inventory -- verify that it exists in inventory??? Need a for loop to loop through each item? 
+function verifyProduct(answer3) {
+  connection.query('SELECT * FROM products WHERE ?', { id: answer3.itemId }, function (err, res) {
+    if (err) throw err;
+    // console.log(res);
+    // console.log(`id ${res[0].id}, product ${res[0].product_name}`)
+    if (res.length === 0) {
+      console.log('Not a valid id')
+      productTable();
+      // connection.end()
+    }
+    else
+      productQuantity(res[0])
+  })
+
+}
+
+// Customer purchase - this should update database and show the total cost of products purchased.
+function productPurchase(data, purchaseQuanity) {
+  // console.log('data', data);
+  console.log('Purchase Quantity:', purchaseQuanity);
+  connection.query(
+    'UPDATE products SET stock_quantity = stock_quantity - ? WHERE ?',
+    [
+
+      purchaseQuanity,
+
+      {
+        id: data.id
+      }
+    ], function (err, res) {
+      if (err) throw err;
+      console.log('Total Cost: $', purchaseQuanity * data.price);
+      connection.end();
+      // productTable();
+      // console.log(`id ${res[0].id}, product ${res[0].product_name}`)
+
+    })
+
+}
---
+++ bamazonSchema.sql
@@ -0,0 +1,51 @@
+-- DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2)NOT NULL,
+	stock_quantity INT NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Go Pro', 'Electronics', 400.00, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Mittens', 'Apparel', 21.95, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Nespresso', 'Kitchen', 395.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone', 'Electronics', 999.00, 600);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Broom', 'Cleaning', 20.00, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Space Heater', 'Home', 30.00, 400);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Towel Warmer', 'Home', 150.00, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Minecraft', 'Video Games', 20.00, 300);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Sled', 'Outdoors', 25.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Nerf Gun', 'Games', 20.00, 100);
+ 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Snow Boots', 'Shoes', 50.00, 100);
+ 
+
+
+
+SELECT * FROM products;
